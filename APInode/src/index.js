40f017f06d6da43b3bb6442e001ec2e843de00da
Victor <adsvictor@outlook.com>
2019-01-14 11:08:41 -0200

    Dados mockados

--- APInode/src/index.js
+++ APInode/src/index.js
@@ -8,9 +8,29 @@ app.get('/data', (req, res) => {
 
     res.send([{
         "id": 1,
-        "firstName": "Victor",
+        "firstName": "Carlos",
+        "lastName": "Moura",
+        "participation": 5
+    }, {
+        "id": 2,
+        "firstName": "Fernanda",
+        "lastName": "Oliveira",
+        "participation": 15
+    }, {
+        "id": 3,
+        "firstName": "Hugo",
         "lastName": "Silva",
-        "participation": 7
+        "participation": 20
+    }, {
+        "id": 4,
+        "firstName": "Eliza",
+        "lastName": "Souza",
+        "participation": 20
+    }, {
+        "id": 5,
+        "firstName": "Anderson",
+        "lastName": "Santos",
+        "participation": 40
     }])
 })
 
